77c30a72a95304b8f52b0dda472323d62d86c297
jekoGetMan <sl1mvsshady@gmail.com>
2018-09-23 23:51:04 +0300

    00

--- 42_piscine/d00/ft_print_alphabet.c
+++ 42_piscine/d00/ft_print_alphabet.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(void)
+int		ft_putchar(char c)
 {
-	printf("Hello world!\n");
-	return 0;
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void){
+	char index;
+	index = 'a';
+	while(index <= 'z')
+	{
+		ft_putchar(index);
+		index++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
 }
