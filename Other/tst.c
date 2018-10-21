89da5b29461bc9a5c14ef69ae925015994c50810
jekoGetMan <sl1mvsshady@gmail.com>
2018-10-21 20:28:13 +0300

    massiv

--- Other/tst.c
+++ Other/tst.c
@@ -1,16 +1,19 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int	main(void){
+void ft_putchar(int display)
+{
+	write(1, &display, 1);
+}
 
-	unsigned short int i;
-	unsigned short int j;
+int	main(void)
+{
+	short int array[4] = {'1', '2', '3', '4'};
+	short int i;
 
-	i = 1;
-	j = 1;
-
-	for (i = 1; i < 3; i++){
-		printf("i: %d, ", i);
-			for (j = 1; j <= 3; j++)
-				printf("j: %d, ", j);
+	i = 0;
+	while (i < 4)
+	{
+		ft_putchar(array[i]);
+		i++;
 	}
 }
